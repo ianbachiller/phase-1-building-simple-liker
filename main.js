bcc0e37c7490c90e77826645d56b52f219889fd5
Ian Bachiller <ianbachise@gmail.com>
2024-02-14 20:38:00 -0500

    complete simple liker lab

--- main.js
+++ main.js
@@ -3,11 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const whoSays = document.querySelectorAll(".like-glyph")
+console.log(whoSays)
+
+for (let heart of whoSays){
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+    .then((resp) => {
+    if(resp === "Pretend remote server notified of action!"){
+      const errorMessage = document.querySelector("#modal")
+      errorMessage.classList.add("hidden")
+      if(heart.innerText === FULL_HEART){
+        heart.innerText = EMPTY_HEART
+      }else{
+        heart.innerText = FULL_HEART
+      }
+      console.log(resp)
+      }
+    })
+    .catch((resp) => {
+      if(resp === "Random server error. Try again."){
+        const errorMessage = document.querySelector("#modal")
+        errorMessage.classList.remove("hidden")
+        console.log(resp)
+      }
+    })
+  })
+}
 
 
-
-
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------÷
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
